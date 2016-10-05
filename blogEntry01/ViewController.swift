537e3a46e9ae84f25ac1875305ce2437d2c9a973
VeloPresso <sebastian.walter1990@gmail.com>
2016-10-05 09:33:32 +0200

    Initial Commit

---
+++ blogEntry01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blogEntry01
+//
+//  Created by Sebastian on 05/10/16.
+//  Copyright © 2016 org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
